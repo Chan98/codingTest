af3c0370fb2ad612931782547a87fb56c6638db4
Chan Park <51689987+Chan98@users.noreply.github.com>
2022-06-24 00:53:07 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        String id = in.next();//공백 포함하지 않는 스트링 입력받음
+        
+        System.out.println(id + "??!");
+        
+        in.close();
+    }
+}
